1b36a170261944ab2fb45b1d6a9bc6d0d47a11eb
Katherine Kokkinias <96079512+Kokkinias@users.noreply.github.com>
2023-11-21 11:41:36 -0700

    Update Figure_3.R

--- Figures/Figure 3/Figure_3.R
+++ Figures/Figure 3/Figure_3.R
@@ -6,16 +6,12 @@ library(pheatmap)
 library(RColorBrewer)
 
 #Data
-setwd("/Users/katherinekokkinias/Desktop/Research/Microbiome/Salmonella/Figures/SalmonellaPaper/R_code_Salmonellapaper/github/Figure3/")
+setwd("path")
 
-getmm = read_excel("Figure3_data.xlsx", sheet="getmm", col_names = TRUE)
-getmm = read_excel("Figure3_data.xlsx", sheet="getmmlog1", col_names = TRUE)
-getmm = read_excel("Figure3_data.xlsx", sheet="getmm_relabun", col_names = TRUE)
 getmm = read_excel("Figure3_data.xlsx", sheet="mean_getmm_relabun", col_names = TRUE)
 
 #without day -1/no sal
 df2 <-getmm[,10:ncol(getmm)]
-df2 <-getmm[,14:ncol(getmm)]
 df2 = as.data.frame(sapply(df2, as.numeric))
 
 pheatmap(df2, scale= "row",cluster_cols =FALSE, cluster_row = FALSE,colorRampPalette(brewer.pal(9,"Reds"))(100))
